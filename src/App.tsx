bbbfbfed2bfb7066d6e16b38cda9c57dd0d62287
“tanya-ermlva” <hi@ermolaeva.co>
2025-02-24 01:43:59 +0000

    removed routing to chat app, added routing to cmdK

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,20 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import { RootLayout } from "./layouts/RootLayout"
 import { BasicLayout } from "./layouts/BasicLayout"
-import { HomePage } from './components/home/HomePage'
-import { ChatPage } from './components/chat/ChatPage'
-import { HelpPage } from './components/help/HelpPage'
-import { NewsPage } from './components/news/NewsPage'
-import { CommandKDemo } from "./components/pages/CommandKDemo" 
+import { CommandKDemo } from "./components/pages/CommandKDemo"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <RootLayout />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />
-      },
-      {
-        path: "/messages",
-        element: <ChatPage />
-      },
-      {
-        path: "/help",
-        element: <HelpPage />
-      },
-      {
-        path: "/news",
-        element: <NewsPage />
-      }
-    ]
-  },
-  {
-    path: "/cmdk",
     element: (
       <BasicLayout>
         <CommandKDemo />
       </BasicLayout>
     ),
-  },
+  }
 ])
 
 export function App() {
-  return (
-    <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: { port: 3000, open: true, },
+  base: '/',
   // resolve: {
   //   alias: {
   //     "@": path.resolve(__dirname, "./src")
